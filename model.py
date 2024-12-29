3a2b5639152f8e49fad18f5fd47af9dddd6cd708
Akash Gaikwad <akash.gaikwad9945@gmail.com>
2024-12-29 14:31:50 +0530

    delete the file for change in project structure

--- model.py
+++
@@ -1,9 +0,0 @@
-import pickle
-
-def load_model(model_path):
-    with open(model_path, 'rb') as file:
-        model = pickle.load(file)
-    return model
-
-def make_predictions(model, data):
-    return model.predict(data)
